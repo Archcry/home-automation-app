927847bf685de578de5e8829a61865f5eea369a6
Sander Koenders <sanderkoenders@gmail.com>
2022-03-06 12:32:05 +0100

    Add support for runtime environment variables to the project

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,12 @@ import { Login } from "./components/auth/login";
 import "./App.css";
 import { AuthProvider } from "./components/auth/auth-context";
 
+declare global {
+  interface Window {
+    _env_: { [key: string]: string };
+  }
+}
+
 function App() {
   return (
     <AuthProvider>
