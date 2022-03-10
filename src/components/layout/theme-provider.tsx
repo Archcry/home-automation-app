9e7067bd1476198bfcde6a6690b811bd21daef7e
Sander Koenders <sanderkoenders@gmail.com>
2022-03-10 19:08:55 +0100

    Save theme.palette.mode to localStorage

--- src/components/layout/theme-provider.tsx
+++ src/components/layout/theme-provider.tsx
@@ -1,23 +1,44 @@
-import { createTheme, Theme, ThemeProvider as MuiThemeProvider } from '@mui/material';
+import { createTheme, PaletteMode, Theme, ThemeProvider as MuiThemeProvider } from '@mui/material';
 import { createContext, ReactElement, useContext, useState } from 'react';
 
 const defaultTheme = createTheme({
   palette: {
-    mode: 'dark',
+    mode: 'light',
   },
 });
 
 export const ThemeContext = createContext({
-  theme: { ...defaultTheme },
+  theme: defaultTheme,
   toggleDarkMode: () => {},
 });
 
 export const useThemeContext = () => useContext(ThemeContext);
 
 export const ThemeProvider = ({ children }: { children: ReactElement }) => {
-  const [theme, setTheme] = useState<Theme>(defaultTheme);
+  const getPaletteModeFromLocalStorage = () => {
+    const mode = localStorage.getItem('theme.palette.mode');
+
+    if (['light', 'dark'].includes(mode ?? '')) {
+      return mode as PaletteMode;
+    }
+
+    return undefined;
+  };
+
+  const [theme, setTheme] = useState<Theme>(
+    createTheme({
+      ...defaultTheme,
+      palette: {
+        mode: getPaletteModeFromLocalStorage() || 'light',
+      },
+    })
+  );
 
   const toggleDarkMode = () => {
+    const newMode = theme.palette.mode === 'light' ? 'dark' : 'light';
+
+    localStorage.setItem('theme.palette.mode', newMode);
+
     setTheme(
       createTheme({
         ...theme,
