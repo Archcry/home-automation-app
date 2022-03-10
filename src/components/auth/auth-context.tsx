732576a88ff1b35fb5ac2b5eea26ce928aac838e
Sander Koenders <sanderkoenders@gmail.com>
2022-03-10 18:55:43 +0100

    Use cookies in auth-context

--- src/components/auth/auth-context.tsx
+++ src/components/auth/auth-context.tsx
@@ -1,28 +1,31 @@
+import Cookies from 'js-cookie';
 import { createContext, ReactChild, useContext, useState } from 'react';
 
 interface AuthContextType {
   username?: string;
   password?: string;
   login: (username: string, password: string) => void;
+  logout: () => void;
 }
 
-const AuthContext = createContext<AuthContextType>({
+export const AuthContext = createContext<AuthContextType>({
   username: undefined,
   password: undefined,
   login: () => {},
+  logout: () => {},
 });
 
 export const useAuthContext = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }: { children: ReactChild }) => {
-  const [credentials, setCredentials] = useState<Omit<Partial<AuthContextType>, 'login'> | null>({
-    username: localStorage.getItem('username') ?? undefined,
-    password: localStorage.getItem('password') ?? undefined,
+  const [credentials, setCredentials] = useState<Pick<AuthContextType, 'username' | 'password'> | null>({
+    username: Cookies.get('username') ?? undefined,
+    password: Cookies.get('password') ?? undefined,
   });
 
   const login = (username: string, password: string) => {
-    localStorage.setItem('username', username.toLowerCase());
-    localStorage.setItem('password', password);
+    Cookies.set('username', username.toLowerCase());
+    Cookies.set('password', password);
 
     setCredentials({
       username: username.toLowerCase(),
@@ -30,5 +33,12 @@ export const AuthProvider = ({ children }: { children: ReactChild }) => {
     });
   };
 
-  return <AuthContext.Provider value={{ ...credentials, login }}>{children}</AuthContext.Provider>;
+  const logout = () => {
+    Cookies.remove('username');
+    Cookies.remove('password');
+
+    setCredentials(null);
+  };
+
+  return <AuthContext.Provider value={{ ...credentials, login, logout }}>{children}</AuthContext.Provider>;
 };
