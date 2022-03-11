bbfca503bf0c0c6cae45a9c0ca18ce0289b05a47
Sander Koenders <sanderkoenders@gmail.com>
2022-03-11 21:40:41 +0100

    Add types for schedules

--- src/pages/somfy/types.ts
+++ src/pages/somfy/types.ts
@@ -15,3 +15,43 @@ export interface Command {
   name: CommandName;
   parameters: string[];
 }
+
+export interface BaseSchedule {
+  uid: string;
+  dow: string[];
+  deviceGroups: DeviceGroup[];
+  command: Command;
+}
+
+export interface FixedTimeSchedule extends BaseSchedule {
+  type: 'fixed_time';
+  time: string;
+  timezone: string;
+}
+
+export type SunCalcKind =
+  | 'sunrise'
+  | 'sunriseEnd'
+  | 'goldenHourEnd'
+  | 'solarNoon'
+  | 'goldenHour'
+  | 'sunsetStart'
+  | 'sunset'
+  | 'dusk'
+  | 'nauticalDusk'
+  | 'night'
+  | 'nadir'
+  | 'nightEnd'
+  | 'nauticalDawn'
+  | 'dawn';
+
+export interface SunCalcSchedule extends BaseSchedule {
+  type: 'suncalc';
+  kind: SunCalcKind;
+  coordinates: {
+    latitude: number;
+    longitude: number;
+  };
+}
+
+export type Schedule = FixedTimeSchedule | SunCalcSchedule;
